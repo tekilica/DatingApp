8c185da9ff021e9f2bf03763c2f76c0dbc20b672
SIMCON-VM\Creen <Milo Stankovi>
2023-03-08 00:39:49 +0100

    End of section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int AppUserId { get; set; }
+        public string UserName { get; set; }
+        
+    }
+}
